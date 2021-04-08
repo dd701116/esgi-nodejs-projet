a89c6d6fe28a2015d9f4045990d87a9226f1c3ac
THOMAS B <tbrunet72@gmail.com>
2021-04-08 12:30:32 +0200

    Revert "Merge pull request #22 from dd701116/route-patch-note"
    This reverts commit 6ba93541761e93696e3ffa303908f166eb925a7a, reversing
    changes made to c6cdcdb57c14514a7a2104e1da967ae4ce316cf0.
    
    	modified:   controller/note.controller.js
    	modified:   routes/note.route.js

--- controller/note.controller.js
+++ controller/note.controller.js
@@ -1,14 +1,13 @@
 const note = require('../models/note.model');
 const customError = require('../models/CustomError');
 const mongodb = require("../db/client.db");
-const ObjectId = require('mongodb').ObjectID;
 
 
 exports.create = async (req, res) => {
     
     //Si pas de body
     if (!req.body || !req.body.content) {
-        throw new customError("Le contenu est requis", 401);
+        return res.send(new customError("Le contenu est requis", 401));
     }
     const content = req.body.content;
     
@@ -24,41 +23,3 @@ exports.create = async (req, res) => {
         throw new customError("Impossible de rajouter la note", 402);
     }
 };
-
-
-exports.change = async (req, res) => {
-    const uIdTest = 1234;
-
-    if (!req.params || !req.params.id) {
-        throw new customError("L'id de la note est requis", 403);
-    }
-    if (!req.body || !req.body.content) {
-        throw new customError("Le nouveau contenu de la note est requis", 403);
-    }
-
-    const noteId = req.params.id;
-    const content = req.body.content;
-    let client = mongodb.getConnection();
-    let filter;
-
-
-    try{
-        filter = {  "_id" :ObjectId(noteId)};
-    }catch(e){
-        throw new customError("L'id n'est pas valable", 406);
-    }
-
-    const updateDoc = {
-        $set: {
-            content : content,
-            lastUpdateAt : Date.now()
-        }
-    };
-
-    try{
-        const result = await client.db("esgi").collection("note").findOneAndUpdate(filter, updateDoc, {returnNewDocument : true, returnOriginal: false});
-        return res.send(result.value);
-    }catch(e){
-        throw new customError("Impossible de modifier la note", 402);
-    }
-}
--- routes/note.route.js
+++ routes/note.route.js
@@ -5,7 +5,9 @@ module.exports = fastify => {
     //Créer la note
     fastify.put('/notes', noteController.create);
 
-    fastify.patch('/notes/:id', noteController.change);
+    fastify.patch('/notes/:id', (req, res) => {
+        res.send('Mettre a jour la note')
+    });
 
     fastify.delete('/notes/:id', (req, res) => {
         res.send('Supprimer la note');
