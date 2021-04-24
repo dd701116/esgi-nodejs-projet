c273c9196083c13a93996512357047ea7918c1c7
Dieunelson Dorcelus <43044653+dd701116@users.noreply.github.com>
2021-04-25 00:29:39 +0200

    feat(user.route): add signip route

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,4 +2,5 @@ const userController = require('../controllers/user.controllers');
 
 module.exports = fastify => {
     fastify.post('/signin', userController.signin);
+    fastify.post('/signup', userController.signup);
 }
