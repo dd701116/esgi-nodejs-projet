1bf46822c210d215df20a94359a4b94993e06ad3
Thomas <tbrunet72@gmail.com>
2021-04-24 15:53:13 +0200

    utilisation de bcrypt pour le password

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -2,6 +2,7 @@ const CustomError = require('../models/CustomError');
 const mongodb = require('../db/client.db');
 const token = require('../models/Token');
 const config = require('../config.default.json');
+const bcrypt = require('bcrypt');
 //Permet a un utilisateur de s'identifier
 
 exports.signin = async (req, res) => {
@@ -24,7 +25,9 @@ exports.signin = async (req, res) => {
             throw new CustomError("Username incorrect", 407);
         }
 
-        if (result.password !== user.password){
+        const samePassword = await !bcrypt.compare(user.password, result.password);
+
+        if (!samePassword){
             throw new CustomError("Mot de passe incorrect", 408);
         }
 
