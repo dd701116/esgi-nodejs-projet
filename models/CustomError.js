16b293232082b8249c475304dc9e14cb3be82a4b
Dieunelson Dorcelus <43044653+dd701116@users.noreply.github.com>
2021-04-29 00:28:16 +0200

    feat: create ErrorFactory

--- models/CustomError.js
+++ models/CustomError.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const locales = {
+  EN:JSON.parse(fs.readFileSync("locales/en.json")),
+  FR:JSON.parse(fs.readFileSync("locales/fr.json"))
+};
+
 class CustomError extends Error{
   constructor(message, code) {
     super(message);
@@ -5,4 +11,17 @@ class CustomError extends Error{
   }
 }
 
-module.exports = CustomError;
+const ErrorFactory = (name, lang="FR") => {
+  let locale;
+  if (/^[a-zA-Z]*\.[a-zA-Z]*$/.test(name)) {
+    let section = name.split(".");
+    locale = locales[lang].error[section[0]][section[1]];
+  }else{
+    locale = locales[lang].error[name];
+  }
+  return new CustomError(locale.message, locale.code);
+}
+module.exports = {
+  CustomError,
+  ErrorFactory
+};
